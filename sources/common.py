31cbb9984126c7c22c851f5db5241f943b2d3e8a
RageYL <vic1708@gmail.com>
2013-11-11 11:48:38 +0100

    no camel

--- sources/common.py
+++ sources/common.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2
 # coding: utf-8
 
-def splitList(list_, chunksize = 2):
+def split_list(list_, chunksize = 2):
   return [list_[i:i + chunksize] for i in xrange(0, len(list_), chunksize)]
